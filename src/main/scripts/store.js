38407495c2f9806c91ff4128fb500dd46627cf8e
emilio <emiliocortina@gmail.com>
2019-03-02 12:08:07 +0100

    First attempt to write on solid pod (failed)

---
+++ src/main/scripts/store.js
@@ -0,0 +1,6 @@
+var rdf = require('rdflib');
+var store = module.exports = rdf.graph(); // Make a Quad store
+rdf.fetcher(store); // Attach a web I/O module, store.fetcher
+store.updater = new rdf.UpdateManager(store); // Add real-time live updates store.updater
+
+console.log('Unique quadstore initialized.');
