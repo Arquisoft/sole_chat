98997cf9c09014ebd034c9dba05809a8e8d99c86
Pabloferpe <47246335+Pabloferpe@users.noreply.github.com>
2019-03-13 09:26:54 +0100

    Boolean test

--- test/experimentalTest.js
+++ test/experimentalTest.js
@@ -1,26 +1,6 @@
-import { Person } from '../src/person.js'
-import { describe, it } from 'mocha'
-import { assert, expect } from 'chai'
+//Simple test that checks if "true" is equal to "true"
+const assert = require('assert');
 
-describe('person', function () {
-  it('has default age of 0', function () {
-    const foo = new Person('foo')
-    assert.equal(foo.age, 0)
-  })
-
-  it('can set age', function () {
-    const foo = new Person('foo')
-    foo.setAge(3)
-    assert.equal(foo.age, 3)
-  })
-
-  it('raises exception when passing negative age', function () {
-    const foo = new Person('foo')
-    expect(() => foo.setAge(-4)).to.throw('Cannot assign negative age')
-  })
-
-  it('keeps its name', function () {
-    const foo = new Person('foo')
-    assert.equal(foo.name, 'foo')
-  })
-})
+it('should return true', () => {
+  assert.equal(true, true);
+});
