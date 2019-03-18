a5e5bc860d0ea102e0bce6b82f0b2df01df058f0
astrid <UO258194@uniovi.es>
2019-03-18 19:52:36 +0100

    dummy message data

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -16,7 +16,18 @@ declare function createFolder(path, folder): any;
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-
+  dummymess = [
+    { id: 11, content: 'Mr. Nice' },
+    { id: 12, content: 'Narco' },
+    { id: 13, content: 'Bombasto' },
+    { id: 14, content: 'Celeritas' },
+    { id: 15, content: 'Magneta' },
+    { id: 16, content: 'RubberMan' },
+    { id: 17, content: 'Dynama' },
+    { id: 18, content: 'Dr IQ' },
+    { id: 19, content: 'Magma' },
+    { id: 20, content: 'Tornado' }
+  ];
   profile: SolidProfile;
   loadingProfile: Boolean;
   messageContent: String = "";
