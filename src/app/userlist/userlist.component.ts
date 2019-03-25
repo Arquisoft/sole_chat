ef0c0415dccc463a8a1d0723353acc5da75dac1f
Javier Garcia Bermudez <javier_garber@yahoo.es>
2019-03-25 16:05:29 +0100

    Freind list

--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -1,30 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { RdfService } from '../services/rdf.service';
 
 @Component({
-  selector: 'app-userlist',
-  templateUrl: './userlist.component.html',
-  styleUrls: ['./userlist.component.css']
+	selector: 'app-userlist',
+	templateUrl: './userlist.component.html',
+	styleUrls: [ './userlist.component.css' ]
 })
-export class UserlistComponent implements OnInit {
+export class UserlistComponent implements OnInit,OnChanges {
   
-  dummyusers = [];
-  constructor(private rdf: RdfService) {
-   this.getUserList()
-   }
+	dummyusers;
+	constructor(private rdf: RdfService) {
+		this.getUserList();
+	}
 
-  ngOnInit() {
-    this.getUserList();
-  }
+	ngOnInit() {
+		this.getUserList();
+	}
 
-  async getUserList() {
-    this.dummyusers=[];
+	async getUserList() {
+		this.dummyusers = [];
     await this.rdf.getFriends(this.dummyusers);
-  
     
   }
-
-
-
+  ngOnChanges() {
+   this.getUserList();
+   console.log("Cambio");
+  }
 
 }
