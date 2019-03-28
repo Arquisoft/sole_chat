2d0476857a73fdff8b8e531f6014922695ddcf03
Pablo <UO258082@uniovi.es>
2019-03-28 22:36:15 +0100

    Solving import problems

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import {beforeEach, describe, it, expect} from '@angular/core/testing/src/testing_internal';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -9,6 +10,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to solid-app!');
+      expect(page.getParagraphText()).toEqual('Welcome to solid-app!');
   });
 });
