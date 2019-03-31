9022fa4ba4e1feacc5ff9668f107fb699de88669
Pablo <UO258082@uniovi.es>
2019-03-31 20:45:28 +0200

    Trying to pass first Cucumber step (Given and When passed, Then failed)

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,23 +1,12 @@
-/*import { browser, by, element } from 'protractor';
 
-export class AppPage {
-  navigateTo() { 
-    return browser.get('/');
-  }
-
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
-}*/
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
     navigateTo() {
-        return browser.get('/');
+        return browser.get('/www.google.com');
     }
 
     getTitleText() {
         return element(by.css('app-root h1')).getText();
     }
 }
- 
