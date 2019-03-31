c78e00cd33a10a43681bb3d9934b4a377df2b355
Javier Garcia Bermudez <javier_garber@yahoo.es>
2019-03-31 13:47:32 +0200

    Friendslist refactor

--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -6,25 +6,54 @@ import { RdfService } from '../services/rdf.service';
 	templateUrl: './userlist.component.html',
 	styleUrls: [ './userlist.component.css' ]
 })
-export class UserlistComponent implements OnInit,OnChanges {
+export class UserlistComponent implements OnInit {
   
 	dummyusers;
+	loadingFriends:boolean;
 	constructor(private rdf: RdfService) {
-		this.getUserList();
+		
 	}
 
 	ngOnInit() {
-		this.getUserList();
+		this.loadingFriends = true;
+		this.loadFriends();
+	
+		// Clear cached profile data
+		// TODO: Remove this code and find a better way to get the old data
+		localStorage.removeItem('oldFriendsData');
+
+
+
+		//this.getUserList();
 	}
+
+	async loadFriends(){
+		try {
+			this.loadingFriends = true;
+			this.dummyusers = [];
+			await this.rdf.getFriends(this.dummyusers);
+			if (this.dummyusers) {
+				if (!localStorage.getItem('oldFriendsData')) {
+					localStorage.setItem('oldFriendsData', JSON.stringify(this.dummyusers));
+				  }
+			 
+			}
+	  
+			this.loadingFriends = false;
+		
+		  } catch (error) {
+			console.log(`Error: ${error}`);
+		  }
+	}
+	
 
 	async getUserList() {
 		this.dummyusers = [];
     await this.rdf.getFriends(this.dummyusers);
     
   }
-  ngOnChanges() {
-   this.getUserList();
-   console.log("Cambio");
-  }
+
+ 
+
 
 }
