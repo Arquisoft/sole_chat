b497af9cf206879f295aae38fb506436e34c8cb8
astrid <UO258194@uniovi.es>
2019-03-31 14:23:24 +0200

    merging

--- e2e/src/pages/app.po.ts
+++ e2e/src/pages/app.po.ts
@@ -1,6 +1,9 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
+  getTitleText() {
+    return element(by.css('app-root h1')).getText();
+  }
   navigateTo() {
     return browser.get('/');
   }
