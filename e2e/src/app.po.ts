3e9d405dc4f7a02e97f74ffa5cbdbdcef74bac55
Pablo <UO258082@uniovi.es>
2019-03-31 21:51:43 +0200

    Creating Feature2 and Step2 (login button)

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -10,4 +10,7 @@ export class AppPage {
        // return element(by.css('app-root h1')).getText();
         return element(by.css('h1')).getText();
     }
+    getButtonText() {
+        return element(by.css('button')).getText();
+    }
 }
---
+++ e2e/src/features/app2.feature
@@ -0,0 +1,8 @@
+Feature: See login button
+  Display the login button
+
+  Scenario: Login Page
+    Given I am on the login page
+    When I do nothing
+    Then I should see the login button
+
