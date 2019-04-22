f76fb413c900cbe130874bfb623839a2f364de50
astrid <UO258194@uniovi.es>
2019-04-22 21:12:22 +0200

    karma 6.0 <test>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,7 +27,17 @@ import { RouterTestingModule } from '@angular/router/testing';
       TestBed.configureTestingModule({
         imports: [    
           RouterTestingModule, NavComponent, UseritemComponent, UserlistComponent,
-          ChatitemComponent, ChatlistComponent, UseritemComponent,UserlistComponent    
+          ChatitemComponent, ChatlistComponent, UseritemComponent,UserlistComponent,
+          AppComponent,
+            LoginComponent,
+            LoginPopupComponent,
+            DashboardComponent,
+            CardComponent,          
+            ChatComponent,
+            AboutComponent,
+            NavComponent, UseritemComponent, 
+            UserlistComponent, ChatitemComponent, 
+            ChatlistComponent, EmojiPickerComponent    
         ],
         declarations: [
           AppComponent
@@ -43,20 +53,5 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to solid-app!');
-  }));
+  
 });
